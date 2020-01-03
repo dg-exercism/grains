2ad79a8f314be43a82ae4bfc094b4f6d3acd1c7f
davidagitlen <davidagitlen@gmail.com>
2020-01-03 16:01:25 -0700

    Include solution for all but last test

---
+++ grains.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+import BigInt from "./lib/big-integer";
+
+export const square = (integer) => {
+  if (integer < 1 || integer > 64) {
+    throw new Error('square must be between 1 and 64')
+  } else {
+    return integer === 1 ? '1' : String(BigInt(2).pow(integer - 1))
+  }
+};
+
+export const total = () => {
+  let squaresTotal = BigInt(16).pow(16);
+  return String(BigInt(squaresTotal - 1))
+};
